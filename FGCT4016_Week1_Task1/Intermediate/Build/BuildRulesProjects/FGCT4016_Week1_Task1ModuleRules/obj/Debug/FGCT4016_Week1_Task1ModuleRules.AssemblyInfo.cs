317e694f9890ab302fd26b9eb9cf63fae53c8bf0
DragonH0rnet <benmphillips1@gmail.com>
2026-02-02 11:59:20 +0000

    Update

--- FGCT4016_Week1_Task1/Intermediate/Build/BuildRulesProjects/FGCT4016_Week1_Task1ModuleRules/obj/Debug/FGCT4016_Week1_Task1ModuleRules.AssemblyInfo.cs
+++ FGCT4016_Week1_Task1/Intermediate/Build/BuildRulesProjects/FGCT4016_Week1_Task1ModuleRules/obj/Debug/FGCT4016_Week1_Task1ModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FGCT4016_Week1_Task1ModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0eb6ae3eb9a43015466bead859d15dd79f0a3c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FGCT4016_Week1_Task1ModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FGCT4016_Week1_Task1ModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ FGCT4016_Week1_Task1/Intermediate/Build/Win64/UnrealEditor/Inc/FGCT4016_Week1_Task1/UHT/FGCT4016_Week1_Task1.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeFGCT4016_Week1_Task1_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_FGCT4016_Week1_Task1;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_FGCT4016_Week1_Task1()
+	{
+		if (!Z_Registration_Info_UPackage__Script_FGCT4016_Week1_Task1.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/FGCT4016_Week1_Task1",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x4C5E37BF,
+				0x840971DE,
+				METADATA_PARAMS(0, nullptr)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_FGCT4016_Week1_Task1.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_FGCT4016_Week1_Task1.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_FGCT4016_Week1_Task1(Z_Construct_UPackage__Script_FGCT4016_Week1_Task1, TEXT("/Script/FGCT4016_Week1_Task1"), Z_Registration_Info_UPackage__Script_FGCT4016_Week1_Task1, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x4C5E37BF, 0x840971DE));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
