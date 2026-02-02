c0eb6ae3eb9a43015466bead859d15dd79f0a3c0
DragonH0rnet <benmphillips1@gmail.com>
2026-02-02 11:00:56 +0000

    Add base unreal project

---
+++ FGCT4016_Week1_Task1/Source/FGCT4016_Week1_Task1/FGCT4016_Week1_Task1.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FGCT4016_Week1_Task1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FGCT4016_Week1_Task1, "FGCT4016_Week1_Task1" );
